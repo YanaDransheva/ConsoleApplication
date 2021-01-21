b45708ffdd11144ee53fc86486ebf7d44581bf15
YanaDransheva <77777130+YanaDransheva@users.noreply.github.com>
2021-01-21 14:36:36 +0500

    Добавлено консольное приложение
    Тестовое консольное приложение

---
+++ Console/src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+   print("vvedite cheslo a, b")
+   val a = readLine()
+   val b = readLine()
+
+   val c = Integer.parseInt(a) + Integer.parseInt(b)
+
+   print("$c")
+
+}
